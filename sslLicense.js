b7c9c65cfa287446cd30f98fb986135e7ff0bce7
iNoyoka <as1232161@gmail.com>
2019-01-17 12:16:18 +0800

    sslCompleteTest

--- sslLicense.js
+++ sslLicense.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 
 //ssl license
-var keyPath = "ssl/yourkey.pem";
-var certPath = "ssl/yourcert.pem";
+var keyPath = "ssl/private.key";
+var certPath = "ssl/certificate.crt";
 
 var hskey = fs.readFileSync(keyPath);
 var hscert = fs.readFileSync(certPath);
